04025b9dba970e94059d15c4c43eefd52429d411
arif wijanarko <arifpethod@gmail.com>
2023-06-16 08:50:14 +0700

    tugas js 04
    tugas untuk niomic

---
+++ tugas4.js
@@ -0,0 +1,12 @@
+var ido = 157;
+var ode = 160;
+var ole = 172;
+
+if (ido > ode && ido > ole) {
+  console.log("ido siswa paling tinggi", ido, ode, ole);
+} else if (ode > ido && ode > ole) {
+  console.log("ode siswa paling tinggi", ode, ido, ole)
+} else {
+  console.log("ole siswa paling tinggi", ole,ode, ido)
+}
+
